26b1c456b0f4b63b8c60d9e834b88a38ab329b50
SebastianL <wardenoath@hotmail.com>
2024-12-05 11:58:36 +0100

    backup

--- src/app/Generate.js
+++ src/app/Generate.js
@@ -3,30 +3,54 @@ import { NavLink } from "react-router-dom";
 
 const GenerateMovies = () => {
 
-    const [query, setQuery] = useState("");
+    const [searchQuery, setSearchQuery] = useState("");
     const [movies, setMovies] = useState([]);
 
-    function createRandomString(length) {
-        const chars = "abcdefghijklmnopqrstuvwxyz";
-        let result = "";
-        for (let i = 0; i < length; i++) {
-            result += chars.charAt(Math.floor(Math.random() * chars.length));
+    const handleSearch = async (e) => {
+
+        const query = String.fromCharCode(97 + Math.floor(Math.random() * 26));
+
+        setSearchQuery(query);
+
+        console.log(query);
+
+        if (query) {
+            const response = await fetch(
+                `https://api.themoviedb.org/3/search/movie?api_key=5ccffe328dbc79c2bc6ea0a9e9d6fe07&query=${query}`
+            );
+            const data = await response.json();
+            const filteredMovies = data.results.filter(
+                (movie) => movie.title && movie.poster_path && movie.release_date
+            );
+
+            let num = Math.floor(Math.random() * 20)
+            setMovies(filteredMovies[num]);
+        } else {
+            setMovies([]);
         }
-        return result;
 
-    }
-
-    /*  useEffect(() => {
-         const fetchGenerate = async () => {
- 
-         };
- 
-         fetchGenerate();
-     }, []); */
+    };
 
     return (
         <div>
-            asdasdaeda
+
+            <button class='generateButton' onClick={handleSearch}>Generate</button>
+
+            <div className="movies">
+                {movies.map((movie) => (
+                    <div className="movie" key={movie.id}>
+                        <NavLink to={`/add-movie/${encodeURIComponent(movie.title)}/Unknown/${movie.release_date}`}>
+                            <img
+                                className="movie-image"
+                                src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
+                                alt={movie.title}
+
+                            />
+                        </NavLink>
+                        <h3>{movie.title}</h3>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
--- src/app/Movies.js
+++ src/app/Movies.js
@@ -1,10 +1,22 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import rotten from "./rotten.png";
+import { replace } from "react-router-dom";
 
 export default function Movies() {
 
     const movies = useSelector((state) => state.movie)
+
+    const gotoRottenTomatoes = (name) => {
+
+        let urlSubstring = name.replaceAll(" ", "_")
+        urlSubstring = urlSubstring.replaceAll(/[^a-zA-Z_0-9]/g, "")
+        urlSubstring = urlSubstring.toLowerCase()
+        let url = `https://www.rottentomatoes.com/m/${urlSubstring}`
+
+        const newWindow = window.open(url, `_blank`, `noopener,noreferrer`)
+        if (newWindow) newWindow.opener = null
+    }
 
     return (
         <div className="favorite-movie-list">
@@ -18,7 +30,7 @@ export default function Movies() {
                                 <strong>Director:</strong> {movie.director} <br />
                                 <strong>Date:</strong> {movie.date} <br />
                             </div>
-                            <img src={rotten} alt="" />
+                            <img src={rotten} alt="" onClick={() => gotoRottenTomatoes(movie.name)} />
 
                         </li>
 
